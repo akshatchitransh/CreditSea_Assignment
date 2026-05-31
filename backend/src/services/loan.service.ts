ceec425e23d693a997265fb60723781bf51d9807
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-05-31 06:12:34 +0530

    loan service added

---
+++ backend/src/services/loan.service.ts
@@ -0,0 +1,29 @@
+export const calculateLoanDetails = (
+  principal: number,
+
+  tenureDays: number
+) => {
+  const interestRate = 12;
+
+  // Simple Interest
+  const interest =
+    (principal *
+      interestRate *
+      tenureDays) /
+    (365 * 100);
+
+  const totalRepayment =
+    principal + interest;
+
+  return {
+    interestRate,
+
+    interestAmount: Number(
+      interest.toFixed(2)
+    ),
+
+    totalRepayment: Number(
+      totalRepayment.toFixed(2)
+    ),
+  };
+};
